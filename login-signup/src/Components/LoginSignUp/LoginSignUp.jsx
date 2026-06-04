a4d44e771a4a00de1959da4c29e146123606f3c0
Bello Qudus <belloqudus3@gmail.com>
2026-06-04 05:45:35 -0700

    more

---
+++ login-signup/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './LoginSignUp.css'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import person_icon from '../Assets/person.png'
+ const LoginSignUp = () => {
+  return (
+    <div className="container">
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+            <div className="inputs">
+                <div className="input">
+                <img src={person_icon} alt="Person-icon" />
+                <input type="text" placeholder='Name' />
+                </div>
+                <div className="input">
+                <img src={email_icon} alt="email-icon" />
+                <input type="email" placeholder='Email' />
+                </div>
+                <div className="input">
+                <img src={password_icon} alt="password-icon" />
+                <input type="password" placeholder='Password' />
+                </div>
+            </div>
+        
+
+           
+        </div>
+    </div>
+  )
+}
+export default LoginSignUp
+
